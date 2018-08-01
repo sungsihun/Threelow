2db712ac481020e9de852fe19148db6b0d171d69
SiHun Sung <sungsihun@gmail.com>
2018-08-01 18:28:17 -0400

    Initial Commit

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by NICE on 2018-08-01.
+//  Copyright © 2018 NICE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
